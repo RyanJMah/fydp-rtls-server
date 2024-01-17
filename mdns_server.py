ca4e2be5245234210aca4270f1747c1ec819a3cb
Ryan Mah <rmah@uwaterloo.ca>
2024-01-16 22:01:11 -0500

    change back to logging lib

--- mdns_server.py
+++ mdns_server.py
@@ -1,34 +1,41 @@
 # IMPORTANT: make sure to run with sudo if it doesn't work at first (lol)
 
+import sys
 import socket
+import logging
 from time import sleep
 from zeroconf import IPVersion, ServiceInfo, Zeroconf
 
 SERVICE_HOSTNAME = "GuidingLight._mqtt._tcp.local."
 
+logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG)
+
 def main():
-    print("Starting mDNS server...")
+    logger.info("Starting mDNS server...")
 
     info = ServiceInfo(
         "_mqtt._tcp.local.",
         SERVICE_HOSTNAME,
         addresses=[socket.gethostbyname(socket.gethostname())],
-        port=80,
+        port=1883
     )
 
     zeroconf = Zeroconf(ip_version=IPVersion.V4Only)
-    zeroconf.register_service(info)
+    zeroconf.register_service(info, ttl=128)
 
-    print(f"Registered service {SERVICE_HOSTNAME}")
+    logger.info(f"Registered service {SERVICE_HOSTNAME}")
 
     try:
         while True:
-            sleep(0.1)
+            pass
 
     finally:
-        print("Unregistering...")
+        print()
+        logger.info("Unregistering...")
         zeroconf.unregister_service(info)
         zeroconf.close()
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
